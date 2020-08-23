70660986adafca35f29919f5a4ec22b8dc22f0ed
Rolv-Apneseth <rolv.apneseth@gmail.com>
2020-08-23 12:24:17 +0100

    Update pass_check_script.py
    Added docstrings and an if __name__ == "__main__" block

--- pass_check_script.py
+++ pass_check_script.py
@@ -2,9 +2,12 @@ import requests
 import sys
 import hashlib
 
-
-
 def api_request(hash_password):
+    """
+    Sends first 5 characters of an encoded sha1 password to the pwned checker api,
+    which then returns tail ends of encoded passwords which matched the first 5 characters sent.
+    """
+
     url = "https://api.pwnedpasswords.com/range/" + hash_password
     re = requests.get(url)
 
@@ -15,6 +18,10 @@ def api_request(hash_password):
 
 
 def get_count(hashes, hash_to_check):
+    """
+    Counts encoded tail ends of passwords which match given tail end of password being checked.
+    """
+
     hashes = (line.split(":") for line in hashes.text.splitlines())
     for h, count in hashes:
         if h == hash_to_check:
@@ -24,6 +31,14 @@ def get_count(hashes, hash_to_check):
 
 
 def api_check(password):
+    """
+    Encodes password given with SHA1 using the hashlib library.
+    Gets the response from api with only the first 5 characters of the encoded password,
+    so that the whole password is not sent over the internet.
+    From the tail ends of encoded passwords then returned by the api_request, the tail ends which match that of the encoded
+    password are counted and returned as a number.
+    """
+
     sha1 = hashlib.sha1(password.encode("utf-8")).hexdigest().upper()
     first5, tail = sha1[:5], sha1[5:]
     response = api_request(first5)
@@ -35,14 +50,14 @@ def main(args):
     for password in args:
         count = api_check(password)
         if count:
-            print(f"{password} was found {count} times... you should probably change your password")
+            print(f"\n{password} was found {count} times... you should probably change your password")
         else:
-            print(f"{password} was not found. Carry on.")
-    return "Done!"
+            print(f"\n{password} was not found. Carry on.")
+    print("Finished!")
 
 
-
-main(sys.argv[1:])
+if __name__ == "__main__":
+    main(sys.argv[1:])
 
 
 
