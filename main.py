e75a10097fd6e588f69e8f09135240571d04493d
Rolv-Apneseth <rolv.apneseth@gmail.com>
2020-08-23 12:37:37 +0100

    Update main.py
    quick update so main.py imports pass_check_script so that it does not need the same functions defined again

--- main.py
+++ main.py
@@ -1,60 +1,22 @@
 import tkinter as tk
 from tkinter import ttk
-import requests
-import hashlib
 
-
-#functions
-
-def pass_api_request(hash_password):
-    url = "https://api.pwnedpasswords.com/range/" + hash_password
-    re = requests.get(url)
-
-    if re.status_code != 200:
-        raise RuntimeError(f"Error fetching: {re.status_code}, check the api and try again.")
-
-    return re
-
-
-def get_count(hashes, hash_to_check):
-    hashes = (line.split(":") for line in hashes.text.splitlines())
-    for h, count in hashes:
-        if h == hash_to_check:
-            return count
-    return 0
-
-
-def pass_api_check(password):
-    sha1 = hashlib.sha1(password.encode("utf-8")).hexdigest().upper()
-    first5, tail = sha1[:5], sha1[5:]
-    response = pass_api_request(first5)
-    return get_count(response, tail)
-
+import pass_check_script
 
 def pass_check(password):
     if password == "":
         console["text"] = "Please enter a password first"
-        return 0
+        return None
+
     entry.delete(0, "end")
+
     console["text"] = f"Checking if {password} has been pwned..."
-    count = pass_api_check(password)
+
+    count = pass_check_script.api_check(password)
     if count:
         console["text"] = f"Check complete\n\nThe password was found: {str(count)} times\n\nA new password is recommended"
     else:
         console["text"] = "Check complete\n\nThe password was not found\n\nA new password is not required"
-
-
-#Email checking requires an api key from Have I Been Pwned api
-##################
-# def email_check(email):
-#     url = "https://haveibeenpwned.com/api/v2/breachedaccount/" + str(email) + "?includeUnverified=true"
-#     re = requests.get(url, verify=True)
-#
-#     if re.status_code != 200:
-#         raise RuntimeError(f"Error fetching: {re.status_code}, check the api and try again.")
-#     else:
-#         print(re)
-################
 
 
 #start of gui
