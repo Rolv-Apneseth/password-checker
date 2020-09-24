376b58579dce44bdb7b120c0dee2ef735060198f
Rolv-Apneseth <rolv.apneseth@gmail.com>
2020-09-24 18:02:35 +0100

    Restructure
    Also added an assets folder and os to imports on main.py for better file pathing to icon.ico

--- password-checker/main.py
+++ password-checker/main.py
@@ -1,7 +1,9 @@
+import os
 import tkinter as tk
 from tkinter import ttk
 
 import pass_check_script
+
 
 def pass_check(password):
     if password == "":
@@ -19,44 +21,49 @@ def pass_check(password):
         console["text"] = "Check complete\n\nThe password was not found\n\nA new password is not required"
 
 
-#start of gui
+# start of gui
 root = tk.Tk()
 
-#Set icon and title for window
-root.tk.call('wm', 'iconphoto', root._w, tk.PhotoImage(file='./icon.ico'))
+# Set icon and title for window
+root.tk.call('wm', 'iconphoto', root._w, tk.PhotoImage(file=os.path.join(os.getcwd(),
+                                                                         'assets/icon.ico'
+                                                                         )))
 root.title("Pwned Password Checker")
 
-#Canvas to define default window size
+# Canvas to define default window size
 canvas = tk.Canvas(root, height=400, width=400)
 canvas.pack()
 
-#background
+# background
 bg_label = tk.Label(root, bg="black")
 bg_label.place(relwidth=1, relheight=1)
 
-#Making the frames
+# Making the frames
 title_frame = tk.Frame(root)
 frame2 = tk.Frame(root)
 frame3 = tk.Frame(root)
 
-#Placing Frames
+# Placing Frames
 title_frame.place(relwidth=0.9, relheight=0.1, relx=0.05, rely=0.05)
 frame2.place(relwidth=0.9, relheight=0.2, relx=0.05, rely=0.2)
 frame3.place(relwidth=0.9, relheight=0.5, relx=0.05, rely=0.45)
 
-#Title
-title_label = tk.Label(title_frame, text="Pwned Password Checker", font=("Helvetica", 14, "underline", "bold"), bg="Black", fg="yellow")
+# Title
+title_label = tk.Label(title_frame, text="Pwned Password Checker", font=(
+    "Helvetica", 14, "underline", "bold"), bg="Black", fg="yellow")
 title_label.place(relwidth=0.99, relheight=0.9, relx=0.005, rely=0.05)
 
-#entry label
-entry = ttk.Entry(frame2, background="gray", font=("Times", 14), justify="center")
+# entry label
+entry = ttk.Entry(frame2, background="gray",
+                  font=("Times", 14), justify="center")
 entry.place(relwidth=0.6, relheight=0.9, relx=0.005, rely=0.05)
 
-#entry button
-password_submit = ttk.Button(frame2, text="Check password", command=lambda: pass_check(entry.get()), cursor="hand2")
+# entry button
+password_submit = ttk.Button(frame2, text="Check password",
+                             command=lambda: pass_check(entry.get()), cursor="hand2")
 password_submit.place(relwidth=0.385, relheight=0.9, relx=0.61, rely=0.05)
 
-#Console
+# Console
 console = ttk.Label(frame3, background="black", foreground="yellow", text="The program is ready for use.\n\nPlease enter a password above and\nclick the 'Check password' button to check\nif your password has been pwned.\n\nDon't worry, as only a small, encrypted\nfragmentof your password is sent over\nthe web, so your password is secure.", justify="left", font=("Helvetica", 14), pad=10, anchor="nw")
 console.place(relwidth=0.99, relheight=0.99, relx=0.005, rely=0.005)
 
